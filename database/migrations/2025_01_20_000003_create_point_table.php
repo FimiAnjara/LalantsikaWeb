9edc9ba63ff0dc5f0ff82984168cd582e083605e
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2026-01-20 10:04:10 +0300

    init project

--- database/migrations/2025_01_20_000003_create_point_table.php
+++ database/migrations/2025_01_20_000003_create_point_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,9 +14,11 @@ return new class extends Migration
     {
         Schema::create('point', function (Blueprint $table) {
             $table->id('id_point');
-            $table->point('coordonnee');
             $table->timestamps();
         });
+        
+        // Add geography column using raw SQL for PostGIS
+        DB::statement('ALTER TABLE point ADD COLUMN coordonnee geography(POINT, 4326) NOT NULL');
     }
 
     /**
